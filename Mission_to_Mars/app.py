090cc79f5cfba66f4c1c18a70ec0c6049fbd9ade
juliemcbaker <83370545+juliemcbaker@users.noreply.github.com>
2021-08-15 16:22:45 -0500

    Update app.py
    working on app updates

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,47 +1,42 @@
 from flask import Flask, render_template, redirect
 import pymongo
+import scrape_mars
 
 ################################################################
 ##          FLASK SETUP
 ################################################################
 app = Flask(__name__)
 
-#############################################################
-##  FLASK ROUTES
-############################################################
-############# pymongo ###########
-def start_mongo():
+################################
+# PYMONGO SETUP
+#################################
     conn = 'mongodb://localhost:27017'
     client = pymongo.MongoClient(conn)
     # define the database in Mongo
     db = client.marsDB
     collection = db.mars_info
-    collection.insert()
+
+#############################################################
+##  FLASK ROUTES
+############################################################
+
+
+    
 
 @app.route("/scrape")
 def pull_data():
-    import scrape_mars.py
     return (scrape_mars)
-    return(start_mongo)
+    
 
-    ############# pymongo ###########
-    conn = 'mongodb://localhost:27017'
-    client = pymongo.MongoClient(conn)
-    # define the database in Mongo
-    db = client.marsDB
-    mars = db.mars_info
+
 
     mars.insert(full_dict)
 
 @app.route("/")
+def home():
+    mars_info = collection.find_one()
+    return render_template('index.html', mars_info=mars_info)
 
 
-
-    ############# pymongo ###########
-    conn = 'mongodb://localhost:27017'
-    client = pymongo.MongoClient(conn)
-    # define the database in Mongo
-    db = client.marsDB
-    mars = db.mars_info
-
-    mars.insert(full_dict)
+if __name__ == "__main__"
+    app.run(debug=True)
